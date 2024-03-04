a85ba9c0c8c40b63a5c013000c08232f13ad3ab5
Tore <tore.heiman@sa6trh.se>
2024-03-04 14:09:26 +0100

    fixade listan av användare och började transitionera från slim till erb

--- app.rb
+++ app.rb
@@ -8,8 +8,11 @@ require 'bcrypt'
 enable :sessions
 
 get('/') do
-  if session[:user] = nil
-  slim(:index)
+  if session[:user] == nil
+    slim(:index)
+  else
+    slim(:home)
+  end
 end
 
 get('/login') do
@@ -35,14 +38,14 @@ end
 post('/login') do
   username = params[:username]
   password = params[:password]
-  result = get_user(username)
+  result = get_user_by_name(username)
   pwdigest = result["pwdigest"]
   id = result["id"]
 
   if BCrypt::Password.new(pwdigest) == password
     session[:user] = username
     session[:user_id] = id
-    redirect('/todos')
+    redirect('/')
   else
     "FEL LOSEN"
   end
@@ -52,6 +55,12 @@ def new_user(username, password)
   password_digest = BCrypt::Password.create(password)
   db = SQLite3::Database.new('db/database.db')
   db.execute("INSERT INTO users (username,pwdigest) VALUES (?, ?)", username, password_digest)
+end
+
+def get_users()
+  db = SQLite3::Database.new('db/database.db')
+  db.results_as_hash = true
+  db.execute("SELECT * FROM users")
 end
 
 post('/users/new') do 
@@ -68,11 +77,13 @@ post('/users/new') do
   end
 end
 
-get('/users/:user') do
-  slim(:"users/profile")
+get('/users') do
+  result = get_users()
+  erb(:"users/index",locals:{users:result})
 end
 
-get("/data") do
-  @data = CSV.open("data/MOCK_DATA.csv", headers: :first_row).map(&:to_h)
-  slim(:data)
+get('/users/:user') do
+  user = params[:user]
+  result = get_user_by_id(user)
+  erb(:"users/show",locals:{users:result})
 end
